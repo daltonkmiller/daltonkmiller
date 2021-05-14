7fc0327acfb1a50a2560479c19d85db919b8a714
Dalton K. Miller <deqlaredalton@gmail.com>
2021-05-13 22:55:38 -0500

    temp

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,8 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Layout title="Home">
-        <h1>Home</h1>
-      </Layout>
-    </div>
+    <Layout title="Home">
+      
+    </Layout>
   )
 }
