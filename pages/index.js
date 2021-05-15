276d54493e0213d15a63d1dfa35a63767b51e20b
Dalton K. Miller <deqlaredalton@gmail.com>
2021-05-15 02:58:39 -0500

    Add temp landing page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Layout from './components/layout'
-import Image from 'next/image'
+import { Flex, Box, Image } from 'rebass'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <Layout title="Home">
-      
+      <Flex height="100%" justifyContent="center" alignItems="center">
+        {/* <Image src="../images/outside.gif" style={{position:'fixed', height:'100vh', width:'100vw'}}/> */}
+        <Image src='../images/loading.gif' width="750px"/>
+      </Flex>
     </Layout>
   )
 }
